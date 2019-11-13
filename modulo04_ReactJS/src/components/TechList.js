1774b3276fca850c7bdecf3ddb771d9f1aaec089
Leandro Gioria <legioria@hotmail.com>
2019-11-13 19:02:55 -0300

    Learned componentDidMount, componentDidUpdate

--- modulo04_ReactJS/src/components/TechList.js
+++ modulo04_ReactJS/src/components/TechList.js
@@ -1,14 +1,34 @@
 import React, { Component } from 'react';
+import TechItem from './TechItem';
+import { throws } from 'assert';
 
 class TechList extends Component {
   state = {
     newTech: '',
-    techs: [
-      'Node.js',
-      'ReactJS',
-      'React Native'
-    ]
+    techs: []
   };
+
+  // Executado assim que o componente aparece em tela
+  componentDidMount () {
+    const techs = localStorage.getItem('techs');
+
+    if(techs) {
+      this.setState({techs: JSON.parse(techs)});
+    }
+  }
+
+  // Executado sempre que houver alterações nas props ou estado
+  componentDidUpdate (_, prevState) {
+    // this.props, this.state
+    if(prevState.techs !== this.state.techs) {
+      localStorage.setItem('techs', JSON.stringify(this.state.techs));
+    }
+  }
+
+  // Executado quando o componente deixa de existir
+  componentWillUnmount() {
+
+  }
 
   handleInputChange = e => {
     this.setState({ newTech: e.target.value });
@@ -31,11 +51,12 @@ class TechList extends Component {
       <form onSubmit={this.handleSubmit}>
         <ul>
           {this.state.techs.map(tech => (
-            <li key={tech}>
-              {tech}
-              <button onClick={() => this.handleDelete(tech)} type="button">Remover</button>
-            </li>
-          ))}
+            <TechItem 
+              key={tech} 
+              tech={tech} 
+              onDelete={() => this.handleDelete(tech)}
+              />
+            ))}
         </ul>
         <input 
           type="text" 
