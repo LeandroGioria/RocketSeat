c01162033ecdf89a2f97636fc3fe44e77a34a451
Leandro Gioria <legioria@hotmail.com>
2019-11-13 20:48:46 -0300

    Updated main page style

--- modulo05/src/pages/Main/index.js
+++ modulo05/src/pages/Main/index.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import { FaGithub, FaPlus } from 'react-icons/fa';
 
-import { Title } from './styles';
+import { Container, Form, SubmitButton } from './styles';
 
 export default function Main() {
-  return <Title error={false}>Main</Title>;
+  return (
+    <Container>
+      <h1>
+        <FaGithub />
+        Repositórios
+      </h1>
+
+      <Form onSubmit={() => {}}>
+        <input type="text" placeholder="Adicionar repositório" />
+
+        <SubmitButton disabled>
+          <FaPlus color="#FFF" size={14} />
+        </SubmitButton>
+      </Form>
+    </Container>
+  );
 }
