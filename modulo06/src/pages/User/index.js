377ce9249bfc4810bc56b14459fcfc0a387cdfca
Leandro Gioria <legioria@hotmail.com>
2019-11-15 18:09:19 -0300

    Implemented User and Main screen

--- modulo06/src/pages/User/index.js
+++ modulo06/src/pages/User/index.js
@@ -1,8 +1,85 @@
-import React from 'react';
-import {View} from 'react-native';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { ActivityIndicator } from 'react-native';
+import api from '../../services/api';
+import {
+    Container,
+    Header,
+    Avatar,
+    Name,
+    Bio,
+    Stars,
+    Starred,
+    OwnerAvatar,
+    Info,
+    Author,
+    Title,
+} from './styles';
 
 // import { Container } from './styles';
 
-export default function User() {
-    return <View />;
+export default class User extends Component {
+    static navigationOptions = ({ navigation }) => ({
+        title: navigation.getParam('user').name,
+    });
+
+    static propTypes = {
+        navigation: PropTypes.shape({
+            getParam: PropTypes.func,
+        }).isRequired,
+    };
+
+    state = {
+        stars: [],
+        loading: false,
+    };
+
+    async componentDidMount() {
+        const { navigation } = this.props;
+        const user = navigation.getParam('user');
+
+        this.setState({ loading: true });
+
+        const response = await api.get(`/users/${user.login}/starred`);
+
+        this.setState({
+            stars: response.data,
+            loading: false,
+        });
+    }
+
+    render() {
+        const { navigation } = this.props;
+        const { stars, loading } = this.state;
+        const user = navigation.getParam('user');
+
+        return (
+            <Container>
+                <Header>
+                    <Avatar source={{ uri: user.avatar }} />
+                    <Name>{user.name}</Name>
+                    <Bio>{user.bio ? user.bio : '...'}</Bio>
+                </Header>
+                {loading ? (
+                    <ActivityIndicator />
+                ) : (
+                    <Stars
+                        data={stars}
+                        keyExtractor={star => String(star.id)}
+                        renderItem={({ item }) => (
+                            <Starred>
+                                <OwnerAvatar
+                                    source={{ uri: item.owner.avatar_url }}
+                                />
+                                <Info>
+                                    <Title>{item.name}</Title>
+                                    <Author>{item.owner.login}</Author>
+                                </Info>
+                            </Starred>
+                        )}
+                    />
+                )}
+            </Container>
+        );
+    }
 }
